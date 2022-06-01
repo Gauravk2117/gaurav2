955af12728dbeec9d80bc1b2b5081eacff93d650
Gaurav <gauravk2117@gmail.com>
2022-06-01 19:24:32 +0530

    first commit

---
+++ src/addtwonumbers/Add.java
@@ -0,0 +1,14 @@
+package addtwonumbers;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int a=12;
+		int b=14;
+		int sum;
+		sum =a+b;
+		System.out.print("Added no is:" +sum);
+
+	}
+
+}
